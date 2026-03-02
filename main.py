b236a2a3a109806641fa967c4c17bdb420aada27
BingBlingBoy <ckeiranbarnes@gmail.com>
2026-03-02 22:20:55 +0000

    Added merge argparse

---
+++ main.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+from pypdf import PdfWriter
+
+def valid_filepath(path):
+    if not os.path.exists(path):
+        raise argparse.ArgumentTypeError(f"The path {path}, does not exists")
+    return path
+
+parent_parser = argparse.ArgumentParser(description="PDF tool", add_help=False)
+group = parent_parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f', '--file', help='Provide File to pdfify', type=valid_filepath)
+group.add_argument('-d', '--dir', help='Provide Directory to pdfify', type=valid_filepath)
+
+parser = argparse.ArgumentParser(description="PDF tool")
+subparsers = parser.add_subparsers(dest="command", required=True)
+
+merge_parser = subparsers.add_parser(
+    "merge",
+    parents=[parent_parser],
+    help="Merge multiple PDFs into one"
+)
+
+convert_parser = subparsers.add_parser(
+    "convert",
+    parents=[parent_parser],
+    help="Convert files to PDF"
+)
+
+args = parser.parse_args()
+print(args)
+
+if args.file and not os.path.isfile(args.file):
+    raise IsADirectoryError(f"Expected file but recieved directory, {args.file}")
+
+if args.dir and not os.path.isdir(args.dir):
+    raise NotADirectoryError(f"Expected folder but got a file, {args.dir}")
+
+merger = PdfWriter()
+for pdf in os.listdir(args.dir):
+    fullpath = os.path.join(args.dir, pdf) 
+    merger.append(fullpath)
+merger.write("output.pdf")
+
+# python3 main.py merge -d example
