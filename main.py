a51cf29700702e8bb9304c88799c23fad26d9331
BingBlingBoy <ckeiranbarnes@gmail.com>
2026-03-06 00:05:15 +0000

    Added output argparse

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ def valid_filepath(path):
 
 parent_parser = argparse.ArgumentParser(description="PDF tool", add_help=False)
 group = parent_parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-f', '--file', help='Provide File to pdfify', type=valid_filepath)
-group.add_argument('-d', '--dir', help='Provide Directory to pdfify', type=valid_filepath)
+group.add_argument('-f', '--file', help='Provide File to PDFify', type=valid_filepath)
+group.add_argument('-d', '--dir', help='Provide Directory to PDFify', type=valid_filepath)
+
+parent_parser.add_argument('-o', '--output', help='Provide Output Filepath to PDFify', required=True)
 
 parser = argparse.ArgumentParser(description="PDF tool")
 subparsers = parser.add_subparsers(dest="command", required=True)
@@ -28,7 +30,10 @@ convert_parser = subparsers.add_parser(
 )
 
 args = parser.parse_args()
-print(args)
+
+# Make output file
+os.makedirs(args.output, exist_ok=True)
+OUTPUT_DIR = os.path.join(".", args.output)
 
 if args.file and not os.path.isfile(args.file):
     raise IsADirectoryError(f"Expected file but recieved directory, {args.file}")
@@ -36,10 +41,17 @@ if args.file and not os.path.isfile(args.file):
 if args.dir and not os.path.isdir(args.dir):
     raise NotADirectoryError(f"Expected folder but got a file, {args.dir}")
 
-merger = PdfWriter()
-for pdf in os.listdir(args.dir):
-    fullpath = os.path.join(args.dir, pdf) 
-    merger.append(fullpath)
-merger.write("output.pdf")
+def merge_files():
+    merger = PdfWriter()
+    for pdf in os.listdir(args.dir):
+        fullpath = os.path.join(args.dir, pdf)
+        merger.append(fullpath)
+    merger.write(f"{OUTPUT_DIR}/test.png")
+
+match args.command:
+    case "merge":
+        merge_files()
+    case "convert":
+        print("Convert")
 
 # python3 main.py merge -d example
